f7a7ab204b6a4dd2766247944552dbd4a4f2be3e
Frankkie <christosfrantzolas@gmail.com>
2020-04-10 03:55:29 +0300

    Fixed Scrapping Bug
    Fixed the scrapping bug, likely the result of the bs4 text method. Also,
    added an html parser to remove the warning from BeautifulSoup

--- worldometer_scrapping.py
+++ worldometer_scrapping.py
@@ -28,7 +28,9 @@ def up_to_date(scope="world"):
     for file in os.listdir(".\\data"):
         if file.endswith(".txt"):
             if file.split(".")[0] == str(datetime.date.fromtimestamp(time.time() - 30 * 3600)) + scope:
+                print("Data is up to date!")
                 return True, file
+    print("Update is needed!")
     return False, None
 
 
@@ -92,7 +94,7 @@ def demographic_data(scope="world"):
     # Data is scrapped from this site
     LINK = "https://www.worldometers.info/world-population/population-by-country/"
     response = requests.get(LINK)
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
     table = soup.findAll("tbody")  # Finds the tables where data is extracted from
     rows = [i for i in table[0].findAll("tr")]
     data = []  # Data starts as a list
@@ -136,7 +138,7 @@ def country_daily_data(country):
     # If not, make a special case
     link = "https://www.worldometers.info/coronavirus/country/" + country
     response = requests.get(link)
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
 
     # If this case is true, you need to make a special case for the country you're looking for.
     if "<title>404 Not Found</title>" in str(soup):
@@ -148,11 +150,11 @@ def country_daily_data(country):
     for i, obj in enumerate(js):
         # Locates each graph by its title, which is contained somewhere in the script
         if "text: 'Total Cases'" in str(obj):
-            cases_graph = soup.findAll("script", {"type": "text/javascript"})[i].text.replace('\n', '').split(";")[1]
+            cases_graph = str(obj).replace('\n', '')
         if "text: 'Total Deaths'" in str(obj):
-            deaths_graph = soup.findAll("script", {"type": "text/javascript"})[i].text.replace('\n', '').split(";")[1]
+            deaths_graph = str(obj).replace('\n', '')
         if "text: 'Active Cases'" in str(obj):
-            active_graph = soup.findAll("script", {"type": "text/javascript"})[i].text.replace('\n', '')
+            active_graph = str(obj).replace('\n', '')
 
     # Finds the part that lists the daily numbers, and splits it into a list
     cases = re.findall('data: \[[0-9,]*\]', cases_graph)[0].split(": ")[1][1:-1].split(",")
@@ -185,14 +187,14 @@ def world_daily_data():
     # For the total cases data
     LINK = "https://www.worldometers.info/coronavirus/"
     response = requests.get(LINK)
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
 
     # Locates the graphs we need from the page script
     js = soup.findAll("script", {"type": "text/javascript"})  # All the javascript parts of the page
     for i, obj in enumerate(js):
         # Finds the graph that lists total cases
         if "text: 'Total Cases'" in str(obj):
-            cases_graph = soup.findAll("script", {"type": "text/javascript"})[i].text.replace('\n', '').split(";")[1]
+            cases_graph =str(obj).replace('\n', '')
 
     # Regular exp to extract the data and the values from Highcharts.chart
     values = re.findall('data: \[[0-9,]*\]', cases_graph)[0].split(": ")[1][1:-1].split(",")
@@ -201,12 +203,12 @@ def world_daily_data():
     # For the deaths data
     LINK = "https://www.worldometers.info/coronavirus/coronavirus-death-toll/"
     response = requests.get(LINK)
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
 
     js = soup.findAll("script", {"type": "text/javascript"})
     for i, obj in enumerate(js):
         if "text: 'Total Deaths'" in str(obj):
-            deaths_graph = soup.findAll("script", {"type": "text/javascript"})[i].text.replace('\n', '').split(";")[1]
+            deaths_graph = str(obj).replace('\n', '')
 
     # regular exp to extract the data and the values from Highcharts.chart
     values_d = re.findall('data: \[[0-9,]*\]', deaths_graph)[0].split(": ")[1][1:-1].split(",")
@@ -215,12 +217,12 @@ def world_daily_data():
     # For the active cases data
     LINK = "https://www.worldometers.info/coronavirus/coronavirus-cases/"
     response = requests.get(LINK)
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
 
     js = soup.findAll("script", {"type": "text/javascript"})
     for i, obj in enumerate(js):
         if "text: 'Active Cases'" in str(obj):
-            active_graph = soup.findAll("script", {"type": "text/javascript"})[i].text.replace('\n', '')
+            active_graph = str(obj).replace('\n', '')
 
     # regular exp to extract the data and the values from Highcharts.chart
     values_a = re.findall('data: \[[0-9,]*\]', active_graph)[0].split(": ")[1][1:-1].split(",")
@@ -242,11 +244,13 @@ def mine_data(scope="world"):
 
     # If not, scrap the new data.
     if not updated:
+        print("Updating demographic data!")
         df_temp = demographic_data(scope=scope)
 
         # If the scope is the entire planet
         if scope == "world":
             world_popul = sum(df_temp.loc[:, "Population"])
+            print("Updating global Covid-19 data!")
             cases, deaths, active_cases = world_daily_data()
 
             # This part calculates the total recovered cases day by day
@@ -259,6 +263,7 @@ def mine_data(scope="world"):
         # If the scope is a single country
         else:
             population = df_temp
+            print("Updating country's Covid-19 data!")
             cases, deaths, active_cases = country_daily_data(scope.lower())
 
             # This part calculates the total recovered cases day by day
