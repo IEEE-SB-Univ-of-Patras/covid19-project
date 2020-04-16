212b7b1f67f3410696d166f2c9281349a3ac1f2f
PapadopoulosLefteris <63300962+PapadopoulosLefteris@users.noreply.github.com>
2020-04-16 19:22:03 +0300

    Add files via upload

--- Prediction_Model.py
+++ Prediction_Model.py
@@ -1,174 +1,313 @@
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-##from sklearn.linear_model import LinearRegression
-from sklearn import linear_model
+from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import MinMaxScaler
 import worldometer_scrapping
 from sklearn.utils import shuffle
 import math
+import sklearn.metrics as met
+from sklearn.linear_model import RidgeCV
+import US_datasets
+import pandas
+import global_data
 
+######################################
+import warnings
+warnings.filterwarnings("ignore")
+######################################
 
+############Settings########
+polynomial_features_check=1
+print_data_check = 0
+regularization_check = 1
+scaling_check = 1
+shuffle_check = 1
+polynomial_order = 25
+print_scores = 0
 
+number_of_elements=2
 
-
-R0 = 2.45  # Basic Reproductive Rate, number of transmissions/infected person in a 100% susceptible population.
-days_of_infectivity = 10  # period during which the infected remain infectious
+R0 = 2.45  
+days_of_infectivity = 10
 recovery_rate = 1 / days_of_infectivity
 transmission_rate = R0 * recovery_rate  
-
-def model():
-
-
-
-##Getting the data from worldometer_scrapping (same code as SIR_prediction        
-    data = worldometer_scrapping.mine_data(scope = 'world')
-
-    n = data["demographics"]["population"]
-
-    total_list = data["cases"]  # The list that contains total confirmed cases day by day.
-    deaths_list = data["deaths"]  # The list of total deaths day by day.
-    R_list = data["recovered"]  # Contains the total non-active (removed/recovered) cases day by day.
-    I_list = data["active"]  # Contains the total active cases (infected/infectious) day by day.
-    S_list = []  # Contains the susceptible population numbers (not infected, not removed).
-    for i, val in enumerate(total_list):
-        S_list.append(n - val)  # Total p
+indx=0
+a=1
+thresh=0
 
 
-#Finding the length of our data (indicies might represent days later)
-    m=len(S_list)
-#Finding the 20% mark to split our data into training and testing sets
-    thresh=int(0.2*m)
+
+
+def settings():
+    global polynomial_features_check
+    global print_data_check
+    global regularization_check
+    global scaling_check
+    global shuffle_check
+    global print_scores
+    global polynomial_order 
+
+
     
-#Initializing the X and Y arrays. X array represents all the data we are gathering
-#and Y represents the values of β. We initialize both of them with zero initial conditions
-#(X array originially contains S, I, R in that order so initializing S is done by equalizing it
-#to the world's population
-    X=np.array([n ,0,0])
-    Y=np.array(0)
+    print("Current settings:\nPolynomial Features ",polynomial_features_check,
+          " Regularization ",regularization_check," Scaling the data",scaling_check,
+          " Shuffiling the data ",shuffle_check," Printing the data ", print_data_check)
+    print("Do you want to change the current settings?(Y/n)")
+    settings_change = input()
 
-#Concatenating the rest of the data
-    for i in range(len(S_list)):
+    if settings_change=='Y':
+        print("Polynomial Features(0 No polynomial features are added/1 polynomial features",
+              "up to the most efficient order are added)")
+        polynomial_features_check = int(input())
 
-#Vertically stacking the S,I,R values from the lists we got from worldometer_scrapping
-        X=np.vstack([X,[S_list[i],I_list[i],R_list[i]]])
+        if polynomial_features_check==1:
+            print("Do you want to print the scores of each case?(Yes=1/No=0)")
+            print_scores = int(input())
 
-#Condition for the first day of the pandemic        
-        if i==0:
-            b=transmission_rate
-#Calculating the rest according to the formula we derived from the SIR model
-        else:
-            b=(S_list[i-1]-S_list[i])*n/(S_list[i-1]*I_list[i-1])
-#Adding the β values to our result vector Y, note that there are real values not estimates            
-        Y=np.vstack([Y,[b]])
-
-
-
-#We could test shuffling the data for better results
-##    X = shuffle(X)
-
-
-#Scaling our data. We need to scale them into smaller values because the difference
-#between S,I and R is usually approximating the world's population
-    min_max_scaler = MinMaxScaler(feature_range=(0,0.5))
-    X_scaled = min_max_scaler.fit_transform(X)
-
-
-#At this point we could try and find out the best order of polynomial equation to use
-#with linear regression
-##    max_score=-math.inf
-##    power=0
-    
-##    for i in range(1,25):
-##        X_temp=X_scaled
-##        
-##        poly = PolynomialFeatures(i)
-##        X_temp = poly.fit_transform(X_scaled)
-##        
-##
-##        X_train=X_temp[:-thresh]
-##        X_test=X_temp[-thresh:]
-##
-##        
-##        
-##
-##        Y_train=Y[:-thresh]
-##        Y_test=Y[-thresh:]
-##
-##          
-##        reg =linear_model.Ridge(alpha=1).fit(X_train,Y_train)
-##        score = reg.score(X_test,Y_test)
-##        print(score,max_score)
-##        if score>max_score:
-##            power = i
-##            max_score = score
-
+            print("Up to what order polynomials would you like to get?(integer, default is 25)")
+            polynomial_order = int(input())
         
-##    print(power,max_score)
+        print("Regularization(0 No regularization checking is done and alpha takes the value of 1",
+              "/1 The best value for alpha is calculated and used")
+        regularization_check = int(input())
 
+        print("Scaling the data(0 no scaling is done/1 data are scaled from 0 to 1)")
+        scaling_check = int(input())
 
-#Creating more features representing the polynomials of our existing data
-    poly = PolynomialFeatures(2)
-    X_scaled= poly.fit_transform(X_scaled)
-    
+        print("Shuffling the data(0 no shuffling is done/1 the data are shuffled)")
+        shuffling_check = int(input())
 
-#Dividing the data into training and testing sets
-    X_train=X_scaled[:-thresh]
-    X_test=X_scaled[-thresh:]
-
-    
-    Y_train=Y[:-thresh]
-    Y_test=Y[-thresh:]
-
-
-#creating our model    
-    reg = linear_model.Ridge()
-
-#Could be used to find the best possible regularization parameter, didn't seem to give
-#better results
-##    reg = linear_model.RidgeCV(alphas=np.logspace(-6, 6, 13))
-
-#Fitting the training set into the model so we get our estimator  
-    reg.fit(X_train,Y_train)
-#Printing the scores on training and testing sets (not sure how the are caluclated)    
-    print(reg.score(X_train,Y_train), reg.score(X_test,Y_test))
-
-#Giving the predictions for the test set
-    Y_pred = reg.predict(X_test)
-
-
-
-
-#Plotting the estimated β value according to each of the values S,I,R
-    plt.subplot(2,2,1)
-
-#Plots for S
-#Plotting the training data (real values)
-    S  = plt.scatter(X_train[:,1],Y_train,color='red', s=1,marker = 'x',linewidths=None)
-#Plotting the predicted values for the test set
-    plt.scatter(X_test[:,1],Y_pred,color = 'black', s=1)
-#Plotting the real values for the test set for comparison
-    plt.scatter(X_test[:,1],Y_test,color = 'teal', s=1)
-
-#Same pattern for I
-    plt.subplot(2,2,2)
-    I = plt.scatter(X_train[:,2],Y_train,color='blue', s=1,marker = 'x',linewidths=None)
-    plt.scatter(X_test[:,2],Y_pred,color = 'black', s=1)
-    plt.scatter(X_test[:,2],Y_test,color = 'teal', s=1)
-
-
-#Same pattern for R
-    plt.subplot(2,2,3)
-    R = plt.scatter(X_train[:,3],Y_train,color='green', s=1,marker = 'x',linewidths=None)
-    plt.scatter(X_test[:,3],Y_pred,color = 'black', s=1)
-    plt.scatter(X_test[:,3],Y_test,color = 'teal', s=1)
-
-
-##    plt.legend((S,I,R),('S','I','R'), loc='upper right')
-    plt.show()
+        print("Print the data(0 no printig is done/1 prints the data)")
+        print_data_check = int(input())
 
     return 0
 
 
-model()
+def print_data(data,results):
+    for i in range(len(data)):
+        print(data[i],results[i])
+
+    return 0
+
+
+
+def data_manipulation(X,Y):
+
+    global thresh
+    global scaling_check
+    global shuffle_check
+    
+    if shuffle_check==1:
+        X,Y =shuffle(X,Y)
+
+    if scaling_check==1:
+        min_max_scaler = MinMaxScaler()
+        X_temp = min_max_scaler.fit_transform(X)
+    else:
+        X_temp=X
+        
+    X_train=X_temp[:-2*thresh]
+    Y_train=Y[:-2*thresh]
+
+    X_CV=X_temp[-2*thresh:-thresh]
+    Y_CV=Y[-2*thresh:-thresh]
+
+    X_test=X_temp[-thresh:]
+    Y_test=Y[-thresh:]
+
+
+
+    return X_train,X_CV,X_test,Y_train,Y_CV,Y_test
+
+
+
+
+def polynomial_checking(X,Y,order_limit=25,print_scores=0):
+
+
+    global indx
+    indx=1
+    global thresh
+    
+    
+    
+    max_score=-math.inf
+    power=1
+
+    
+    
+    for i in range(1,order_limit):
+        X_temp=X
+        
+        poly = PolynomialFeatures(i)
+        X_temp = poly.fit_transform(X)
+        
+
+        X_train=X_temp[:-2*thresh]
+        Y_train=Y[:-2*thresh]
+
+        X_CV=X_temp[-2*thresh:-thresh]
+        Y_CV=Y[-2*thresh:-thresh]
+
+        X_test=X_temp[-thresh:]
+        Y_test=Y[-thresh:]
+
+        
+
+        
+        
+        reg = Ridge()  
+        reg.fit(X_train,Y_train)
+        Y_pred=reg.predict(X_CV)
+
+        
+        score = met.r2_score(Y_CV,Y_pred)
+
+        if print_scores==1:
+            print(i,score)
+        if score>max_score+0.0001:
+            power = i
+            max_score = score
+    print("\nChosen polynomial order is:",power)
+    poly = PolynomialFeatures(power)
+    X_scaled= poly.fit_transform(X)
+                
+    return X_scaled
+
+
+
+def regularization_parameter(X,Y):
+
+
+
+    reg = RidgeCV(alphas = list(np.arange(0.02,1,0.02)))
+    model = reg.fit(X,Y)
+
+    print("Best regularization parameter:",model.alpha_)
+    return model.alpha_
+
+
+
+def model():
+
+    global thresh
+
+    data = worldometer_scrapping.mine_data(scope = 'Greece')
+
+    n = data["demographics"]["population"]
+
+    total_list = data["cases"]  
+    deaths_list = data["deaths"]  
+    R_list = data["recovered"] 
+    I_list = data["active"]  
+    S_list = []  
+    for i, val in enumerate(total_list):
+        S_list.append(n - val)  
+
+
+
+    X=np.array([0]*number_of_elements)
+    Y=np.array(0)
+    
+
+    
+    for i in range(len(S_list)):
+
+        if I_list[i]==0:
+            continue
+
+        else:
+            
+            X=np.vstack([X,[I_list[i],R_list[i]]])
+                
+            
+                
+            if I_list[i-1]==0:
+                b=transmission_rate
+            else:
+                b=(S_list[i-1]-S_list[i])*n/(S_list[i-1]*I_list[i-1]+1)
+                if b>=0.245:
+                    b=0.245
+            Y=np.vstack([Y,[b]])
+
+    
+
+    
+    m=len(Y)
+    thresh=int(0.15*m)
+
+
+
+
+#Polynomial checking    
+    if polynomial_features_check == 1:
+        X= polynomial_checking(X,Y,polynomial_order,print_scores)
+
+
+
+#Scaling, shuffling and division of data    
+    X_train,X_CV,X_test,Y_train,Y_CV,Y_test= data_manipulation(X,Y)
+
+    
+
+
+
+#Printing the data
+    if print_data_check==1:
+        print("Whole Data\n")
+        print_data(X,Y)
+        print("\nTraining Data\n")
+        print_data(X_train,Y_train)
+        print("\nCV Data\n")
+        print_data(X_CV,Y_CV)
+        print("\nTesting Data\n")
+        print_data(X_test,Y_test)
+
+
+
+#Regularization parameter checking
+    if regularization_check==1:
+        a = regularization_parameter(X_CV,Y_CV)   
+    else: a=0
+    
+
+
+
+
+
+#Model training and predictions    
+    reg = Ridge(alpha=a)
+    reg.fit(X_train,Y_train)
+
+    Y_test_pred = reg.predict(X_test)
+    Y_CV_pred = reg.predict(X_CV)
+    Y_train_pred=reg.predict(X_train)
+    
+    
+    print("\nTraining set fitting score:",met.r2_score(Y_train,Y_train_pred),"\nCV set fitting score",
+          met.r2_score(Y_CV,Y_CV_pred),"\nTest set fitting score", met.r2_score(Y_test,Y_test_pred))
+
+
+    
+
+    S  = plt.scatter(X_train[:,indx],Y_train,color='red', s=1,marker = 'x',linewidths=None)
+    plt.scatter(X_test[:,indx],Y_test_pred,color = 'black', s=1)
+    plt.scatter(X_test[:,indx],Y_test,color = 'teal', s=1)
+
+
+
+
+    plt.show()
+
+    return 0
+
+data_set = getattr(global_data,'data')
+x=data_set.get('general_country')
+print(x[x['pop']>'10000000000'])
+
+
+##settings()
+##model()
+
